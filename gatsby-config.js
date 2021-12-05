1656afeba142dd42900450dba5f35044c76ae119
Canhua Li <licanhua@live.com>
2021-12-04 21:22:27 -0800

    add clang-format and react-todo project

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,16 @@ module.exports = {
     {
       resolve: "smooth-doc",
       options: {
-        name: "Smooth DOC Starter",
-        description: "Use your own description...",
-        siteUrl: "https://example.com",
-        navItems: [{ title: 'Docs', url: '/docs/' }],
-        sections: ['Guides']
+        name: "Canhua's GitHub Projects",
+        description: "List open source github projects developed by canhua",
+        siteUrl: "https://licanhua.github.io/github-projects",
+        navItems: [
+          { title: 'Blogs', url: 'https://licanhua.github.io/' },
+          { title: 'GitHub-Projects', url: '/docs/' },
+          { title: 'Profile', url: 'https://licanhua.github.io/profile' },
+          { title: 'Linkedin', url: 'https://www.linkedin.com/in/licanhua/' }
+        ],
+        sections: ['Projects']
       },
     },
   ],
