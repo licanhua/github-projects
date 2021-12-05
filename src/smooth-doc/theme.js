ac5ab75f88176be801dc7ebb3e397aebd0faa261
Canhua Li <licanhua@live.com>
2021-12-05 09:21:49 -0800

    delete public and update theme background

---
+++ src/smooth-doc/theme.js
@@ -0,0 +1,8 @@
+import { theme as baseTheme } from 'smooth-doc/src/theme'
+export const theme = {
+    ...baseTheme,
+    colors: {
+        ...baseTheme.colors,
+        background: '#D0DDE3',
+    },
+}
